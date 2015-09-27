b9b5cfe719f5d6443eeb3f5385b2e69526a842db
robb <marco_417_@hotmail.com>
2015-09-27 00:23:01 -0700

    dasda
    asdsa

--- cicloVida/lifeCycle/app/src/main/java/com/mim/marcoisaac/lifecycle/MainActivity.java
+++ cicloVida/lifeCycle/app/src/main/java/com/mim/marcoisaac/lifecycle/MainActivity.java
@@ -1,18 +1,90 @@
 package com.mim.marcoisaac.lifecycle;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            String nombre = savedInstanceState.getCharSequence("name").toString();
+            if (nombre != null) {
+                Toast.makeText(this, "el nombre es " + nombre, Toast.LENGTH_SHORT).show();
+            }
+        }
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "oncreate seth", Toast.LENGTH_SHORT).show();
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause actividad", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume actividad", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart actividad", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        Toast.makeText(this, "onResumeFrags actividad", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop actividad", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState);
+        Toast.makeText(this, "onRestoreInstanceState recuperar actividad", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putCharSequence("name", "nelva");
+        super.onSaveInstanceState(outState);
+        Toast.makeText(this, "onSaveInstanceState actividad", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Toast.makeText(this, "onPostResume actividad", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart actividad", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy actividad", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
